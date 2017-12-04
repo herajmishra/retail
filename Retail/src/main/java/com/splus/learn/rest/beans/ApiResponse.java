239049d17ea6e7b014579c3d8b2e5c80f96bb7b4
Heraj Mishra <heraj.mishra@spluspl.com>
2017-12-04 12:55:57 +0530

    Added javadoc comments in ApiResponse.

--- Retail/src/main/java/com/splus/learn/rest/beans/ApiResponse.java
+++ Retail/src/main/java/com/splus/learn/rest/beans/ApiResponse.java
@@ -1,31 +1,60 @@
 package com.splus.learn.rest.beans;
 
+/**
+ * This beans is used for sending response to client. 
+ * @author Heraj.Mishra
+ *
+ */
 public class ApiResponse {
 
 	private int code;
 	private Object result;
 	private String message;
 
+	/**
+	 * This methods is for getting code. 
+	 * @return
+	 */
 	public int getCode() {
 		return code;
 	}
 
+	/**
+	 * This method is for setting code. 
+	 * @param code
+	 */
 	public void setCode(int code) {
 		this.code = code;
 	}
 
+	/**
+	 * This method is for getting result.
+	 * @return
+	 */
 	public Object getResult() {
 		return result;
 	}
 
+	/**
+	 * This method is for setting result. 
+	 * @param code
+	 */
 	public void setResult(Object result) {
 		this.result = result;
 	}
 
+	/**
+	 * This method is for getting message.
+	 * @return
+	 */
 	public String getMessage() {
 		return message;
 	}
 
+	/**
+	 * This method is for setting message. 
+	 * @param code
+	 */
 	public void setMessage(String message) {
 		this.message = message;
 	}
